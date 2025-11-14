2e9493d4a08f0c9c9bc5f66b2ccbbba965e6de6e
n3rdyguy <n3rdyguy@gmail.com>
2025-11-14 23:04:49 +0100

    Register sidebar

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+// eslint.config.js
+import antfu from "@antfu/eslint-config"
+
+export default antfu({
+  // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
+  // The `ignores` option in the option (first argument) is specifically treated to always be global ignores
+  // And will **extend** the config's default ignores, not override them
+  // You can also pass a function to modify the default ignores
+  ignores: [
+    "**/fixtures",
+    // ...globs
+  ],
+
+  // Parse the `.gitignore` file to get the ignores, on by default
+  gitignore: true,
+
+  stylistic: {
+    indent: 2, // 4, or 'tab'
+    quotes: "double",
+  },
+
+  // TypeScript and Vue are autodetected, you can also explicitly enable them:
+  typescript: true,
+  vue: true,
+})
---
+++ pages/register.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex bg-black h-screen">
+    <!-- sidebar -->
+    <div class="bg-zinc-900 w-[516px] p-16 flex flex-col justify-center">
+      <Logo />
+      <h1 class="text-white font-bold text-lg mt-8">
+        Sign up for a free account
+      </h1>
+      <p class="text-zinc-300 text-sm mt-0.5 mb-12">
+        Already registered? <span class="font-bold text-[#FFAC00] underline cursor-pointer">Log in</span> to your account
+      </p>
+      <div class="mb-8">
+        <label for="" class="text-zinc-300 text-sm block mb-0.5">Email Address</label>
+        <input id="" placeholder="your@email.com" type="email" name="email" class="w-full bg-[#27272A] border border-[#3F3F46] rounded text-white p-2 placeholder:text-zinc-500 text-sm">
+      </div>
+      <div class="mb-8">
+        <label for="" class="text-zinc-300 text-sm block mb-0.5">Password</label>
+        <input id="" placeholder="**********************" type="password" name="email" class="w-full bg-[#27272A] border border-[#3F3F46] rounded text-white p-2 placeholder:text-zinc-500 text-sm">
+      </div>
+      <div>
+        <button class="flex justify-center gap-2 items-center w-full bg-[#FFAC00] rounded-full px-4 py-2">
+          Sign Up
+          <ArrowRight />
+        </button>
+      </div>
+    </div>
+    <!-- end sidebar -->
+    <!-- note intro -->
+    <!-- end note intro -->
+  </div>
+</template>
