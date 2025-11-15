c74702822be9bc40f9371e64b8cb701570b338e4
n3rdyguy <n3rdyguy@gmail.com>
2025-11-15 18:07:38 +0100

    User create and bcrypt pwd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ["~/assets/main.css"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-})
+
+  modules: ["@prisma/nuxt"],
+});
